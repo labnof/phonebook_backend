cdbe0d39cc664f059f49a933796962edb951c6f6
Anafi Babatunde <anafio1@t31300-lr065.org.aalto.fi>
2020-08-13 12:42:26 +0300

    added morgan(tiny) logger

--- index.js
+++ index.js
@@ -3,46 +3,43 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan)
-
-
-
+app.use(morgan('tiny'))
 
 let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-        "name": "Mary1 Poppendieck1",
-        "number": "394-23-64231220",
-        "id": 5
-      }
-  ]
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  },
+  {
+    name: 'Mary1 Poppendieck1',
+    number: '394-23-64231220',
+    id: 5
+  }
+]
 
 app.get('/', (req, res) => {
   res.send('<h1>Phonebook</h1>')
 })
 app.get('/info', (req, res) => {
-    res.send(`<div><h3>Phonebook has info for ${persons.length}
+  res.send(`<div><h3>Phonebook has info for ${persons.length}
      people.</h3><p>${new Date()}</></div>`)
-  })
+})
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
@@ -65,21 +62,21 @@ app.delete('/api/persons/:id', (request, response) => {
   console.log('Phonebook:', persons)
 })
 const generateId = () => {
-  return  Math.floor(Math.random() * Math.floor(99999999))
+  return Math.floor(Math.random() * Math.floor(99999999))
 }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log('body',body)
+  console.log('body', body)
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number missing'
     })
-  }else if(persons.find(person => person.name === body.name )) {
-            return response.status(412).json({
-              error: 'name already in phonebook'
-          })
-}
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(412).json({
+      error: 'name already in phonebook'
+    })
+  }
   const person = {
     name: body.name,
     number: body.number,
