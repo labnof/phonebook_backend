265750ceb62885d55b52a1ebcac5270163a5daae
Babatund Anafi <labnof@gmail.com>
2020-08-13 14:19:36 +0300

    added exe 3.8

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const morgan = require('morgan')
+// const morganBody = require('morgan-body') 
+// const bodyParser = require('body-parser') 
 const app = express()
 
+
+
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('content', function (req, res) { 
+  body = JSON.stringify(req.body)
+  console.log('body', body);
+  return (body === {}) ? '' : body
+  })// definded morgan token
+const tinyContent = morgan(':method :url :status :res[content-length] - :response-time ms :content')
+app.use(tinyContent)
+
+
+// must parse body before morganBody as body will be logged
+//app.use(bodyParser.json()) 
+// hook morganBody to express app
+//morganBody(app)
+
+
 
 let persons = [
   {
@@ -59,7 +77,7 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
-  console.log('Phonebook:', persons)
+
 })
 const generateId = () => {
   return Math.floor(Math.random() * Math.floor(99999999))
@@ -67,7 +85,6 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log('body', body)
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number missing'
@@ -86,6 +103,7 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
